8f21d8b4fdf67f92497f904788af4859c673d7ee
HibbanHaroon <hibbanharoon@gmail.com>
2023-09-20 02:44:05 +0500

    Added Download Quiz Feature

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from utils.get_audio_file import get_audio_file
 from utils.get_transcription import get_transcription
 from utils.get_quiz_array import get_quiz_array
 import streamlit as st
+from fpdf import FPDF
+import os
 
 # This function takes information from the user - the url 
 def get_information():
@@ -27,10 +29,10 @@ def get_information():
             quiz_array = get_quiz_array(text)
             st.session_state.quiz_array = quiz_array
 
-        show_quiz()
+        show_buttons()
 
-# This function displays the quiz to the user which you can take and then on submit view the results.
-def show_quiz():
+# This function converts the quiz_array which is a string returned from gpt-4 to a list and then save it in session state
+def get_quiz_list():
     quiz_array = st.session_state.quiz_array
 
     # Removing any escape sequences as it hinders in conversion of string to list of the quiz_result
@@ -44,6 +46,97 @@ def show_quiz():
 
     # Convert the string to a list
     quiz_list = eval(quiz_array)
+
+    if "quiz_list" not in st.session_state:
+        st.session_state.quiz_list = []
+
+    st.session_state.quiz_list = quiz_list
+
+    return quiz_list
+
+# Function to generate and download the quiz PDF
+def download_quiz():
+
+    quiz_list = get_quiz_list() 
+
+    # Initialize the PDF object
+    pdf = FPDF()
+
+    pdf.add_page()
+    pdf.set_font("Times", size=24)
+
+    pdf.cell(200, 10, txt="Lumina", ln=1, align='C')
+
+    pdf.line(0, 22, 500, 22)
+
+    pdf.set_font("Times", size=18)
+
+    pdf.cell(200, 10, txt="", ln=1, align='L')
+    pdf.cell(200, 10, txt="Quiz : ", ln=1, align='L')
+
+    # Loop through the quiz list and add questions and their choices
+    for question_data in quiz_list:
+        question_number, question_text, *choices, correct_choice = question_data
+
+        pdf.set_font("Times", size=12)
+
+        # Add the question to the PDF
+        pdf.cell(200, 10, txt=f"Question {question_number}: {question_text}", ln=1, align='L')
+
+        pdf.set_font("Times", size=11)
+
+        # Add the choices to the PDF
+        for choice_number, choice_text in enumerate(choices, start=1):
+            pdf.cell(200, 5, ln=1, txt=f"{choice_number}. {choice_text}", align='L')
+
+    pdf.add_page()
+    pdf.set_font("Times", size=12)
+
+    pdf.cell(200, 10, txt="Answers : ", ln=1, align='L')
+
+    answers = [item[6] for item in quiz_list]
+
+    # Add the answers to the PDF at the end in a new page
+    for answer_number, answer in enumerate(answers, start=1):
+        pdf.cell(200, 5, ln=1, txt=f"{answer_number}. {answer}", align='L')
+
+    # Save the PDF to a file
+    pdf_file_path = "quiz.pdf" 
+    pdf.output(pdf_file_path)
+
+    return pdf_file_path 
+
+# This function hides the Take Quiz and Download PDF buttons
+def hide_buttons():
+    if st.session_state.show_buttons:
+        st.session_state.show_buttons = False
+    
+
+# This function hides the Take Quiz and Download PDF buttons after generating the Quiz
+def show_buttons():
+    # Create a session state to track buttons visibility
+    if "show_buttons" not in st.session_state:
+        st.session_state.show_buttons = True
+
+    # Conditionally show/hide the buttons
+    if st.session_state.show_buttons:
+        st.write("Do you wanna ")
+        st.button("Take the Quiz", type="primary", on_click=hide_buttons)
+        st.write("or")
+        pdf_file_path = download_quiz()  # Generate and get the PDF file path
+        with open(pdf_file_path, "rb") as pdf_file:
+            st.download_button(
+                label="Download Quiz PDF",
+                data=pdf_file.read(),   
+                key="pdf-download",
+                file_name="quiz.pdf", 
+                type="primary",
+            )
+
+
+# This function displays the quiz to the user which you can take and then on submit view the results.
+def show_quiz():
+    quiz_list = st.session_state.quiz_list
 
     # Initializing session states
     if "index" not in st.session_state:
@@ -104,23 +197,26 @@ def show_quiz():
 
     if st.session_state.quiz_submitted == False:
         # Display Submit if it is the last question other display Next button
-        if st.session_state.index < len(st.session_state.questions)-1:
+        if index < length - 1:
             next_button = st.button("Next Question", type="primary", on_click=next)
-        elif st.session_state.index == len(st.session_state.questions)-1:
+        elif index == length - 1:
             button.button("Submit", type="primary", on_click=next)
     else:
         # Display Return to Home button if it is the last question in the view results after quiz submission, otherwise display Next button
-        if st.session_state.index < len(st.session_state.questions)-1:
+        if index < length - 1:
             next_button = st.button("Next Question", type="primary", on_click=next)
-        elif st.session_state.index == len(st.session_state.questions)-1:
+        elif index == length - 1:
             button.button("Return to Home", type="primary", on_click=return_to_home)
 
 def next():
-    if st.session_state.index < len(st.session_state.questions)-1:
+    index = st.session_state.index
+    length = len(st.session_state.quiz_list)
+
+    if index < length - 1:
         st.session_state.index += 1
         # Appending answers that the user inputs
         st.session_state.user_answers.append(st.session_state.latest_answer)
-    elif st.session_state.index == len(st.session_state.questions)-1:
+    elif index == length - 1:
         # Appending the last answer too that the user inputs
         st.session_state.user_answers.append(st.session_state.latest_answer)
         compare()
@@ -167,7 +263,10 @@ def main():
     if st.session_state.quiz_started == False:
         get_information()
     else: 
-        show_quiz()
+        if st.session_state.show_buttons:
+            show_buttons()
+        else: 
+            show_quiz()
 
 
 if __name__ == '__main__':
