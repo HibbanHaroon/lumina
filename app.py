3a4b442b04928b6a822b784b09220c9a7be422e3
HibbanHaroon <hibbanharoon@gmail.com>
2023-09-20 01:41:51 +0500

    Added comments and Removed unnecessary code

--- app.py
+++ app.py
@@ -4,13 +4,17 @@ from utils.get_transcription import get_transcription
 from utils.get_quiz_array import get_quiz_array
 import streamlit as st
 
+# This function takes information from the user - the url 
 def get_information():
     url_container = st.empty()
     url_content_container = url_container.container()
-    url = url_content_container.text_input('URL of Lecture Video', 'https://www.youtube.com/watch?v=rs9AFEebHsk')
+    url = url_content_container.text_input('Youtube URL of Lecture Video', 'https://www.youtube.com/watch?v=rs9AFEebHsk')
     generate_button = url_content_container.button("Generate Quiz", type="primary")
+
     if generate_button:
         st.session_state.quiz_started = True
+
+        # Removing the url container after clicking the generate button
         url_container.empty()
 
         with st.spinner('Converting Video File to Audio...'):
@@ -23,23 +27,13 @@ def get_information():
             quiz_array = get_quiz_array(text)
             st.session_state.quiz_array = quiz_array
 
-        # quiz_array = """[[1, "What are the two pathways that carry information in the nervous system?", "Afferent and Efferent", "Central and Peripheral", "Autonomic and Somatic", "Sympathetic and Parasympathetic", "Afferent and Efferent"],
-        # [2, "What does the Central Nervous System consist of?", "Brain and Spinal Cord", "Heart and Lungs", "Eyes and Ears", "Arms and Legs", "Brain and Spinal Cord"],
-        # [3, "Which system controls our voluntary movements?", "Autonomic System", "Somatic System", "Sympathetic System", "Central System", "Somatic System"],
-        # [4, "What is the arousing system in our body?", "Parasympathetic System", "Peripheral System", "Sympathetic System", "Central System", "Sympathetic System"],
-        # [5, "What type of cells in our nervous system provides support and nutrition?", "Neurons", "Mitochondria", "Glial Cells", "Ribosomes", "Glial Cells"],
-        # [6, "What part of neuron carries the information away from the cell body?", "Dendrites", "Axon", "Myelin", "Terminal Buttons", "Axon"],
-        # [7, "What is the space between two neurons called?", "Axon", "Synapse", "Dendrite", "Myelin", "Synapse"],
-        # [8, "What does an action potential operate by?", "All or None Principle", "First Come First Serve Principle", "Last Come First Serve Principle", "None of the Above", "All or None Principle"],
-        # [9, "What happens in the reuptake process ?", "Transmission of neurotransmitter to another neuron", "Reabsorption of neurotransmitter back into the vesicle", "Neuron firing", "Creation of a new neurotransmitter", "Reabsorption of neurotransmitter back into the vesicle"],
-        # [10, "Which of the following aids in speeding up the transmission in neurons?", "Dendrites", "Axon", "Synapse", "Myelin", "Myelin"]]"""
-
-        # st.session_state.quiz_array = quiz_array
         show_quiz()
 
+# This function displays the quiz to the user which you can take and then on submit view the results.
 def show_quiz():
     quiz_array = st.session_state.quiz_array
-    # Replace any escape sequences
+
+    # Removing any escape sequences as it hinders in conversion of string to list of the quiz_result
     quiz_array = quiz_array.replace("\n", "")
     quiz_array = quiz_array.replace("\t", "")
     quiz_array = quiz_array.replace("\r", "")
@@ -71,6 +65,7 @@ def show_quiz():
 
     st.markdown("## Quiz")
 
+    # If the quiz is submitted, display the results
     if st.session_state.quiz_submitted == True:
         st.markdown(f"### You scored: {st.session_state.no_of_correct_answers}/{len(st.session_state.questions)}")
         user_answer = st.session_state.user_answers[index]
@@ -86,6 +81,7 @@ def show_quiz():
 
     if st.session_state.quiz_submitted == True:
         user_answer = st.session_state.user_answers[index]
+        # Display the answer that user selected upon quiz submission
         st.radio(
             question,
             options,
@@ -101,16 +97,19 @@ def show_quiz():
             key = index,
         )
 
+    # The answer that user selectes the latest (before clicking the Next button)
     st.session_state.latest_answer = user_answer
 
     button = st.empty()
 
     if st.session_state.quiz_submitted == False:
+        # Display Submit if it is the last question other display Next button
         if st.session_state.index < len(st.session_state.questions)-1:
             next_button = st.button("Next Question", type="primary", on_click=next)
         elif st.session_state.index == len(st.session_state.questions)-1:
             button.button("Submit", type="primary", on_click=next)
     else:
+        # Display Return to Home button if it is the last question in the view results after quiz submission, otherwise display Next button
         if st.session_state.index < len(st.session_state.questions)-1:
             next_button = st.button("Next Question", type="primary", on_click=next)
         elif st.session_state.index == len(st.session_state.questions)-1:
@@ -134,7 +133,7 @@ def compare():
     if len(answers) != len(user_answers):
         print("The lists have different lengths, so they are not equal.")
     else:
-        # Use a loop to compare the values at each index
+        # Use a loop to compare the values at each index and count the correct and incorrect answers
         for i in range(len(answers)):
             if answers[i] == user_answers[i]:
                 st.session_state.no_of_correct_answers += 1
@@ -145,7 +144,7 @@ def compare():
     st.session_state.index = 0
 
 def return_to_home():
-    # Delete all the items in Session state
+    # Delete all the items in session state
     for key in st.session_state.keys():
         del st.session_state[key]
 
@@ -154,7 +153,10 @@ def main():
     st.set_page_config(page_title="Lumina", page_icon="🎓")
 
     st.header("Lumina 🎓")
-    st.markdown("Turn educational video lectures into engaging MCQ based quizzes and automatic assessment. Learning has never been this fun!")
+    st.markdown("Lumina project is an AI powered Educational Quiz Generator developed by team **fruitydibs** for the **Streamlit LLM Hackathon**.  Lumina turns your educational video lectures into engaging MCQ based quizzes and provides automatic assessment. Learning has never been this fun!")
+    st.write("Lumina utilizes the transcription feature of **AssemblyAI** and transcribes a video such as a video lecture from youtube and then gives the transcribed text to GPT-4 Model from **Clarifai** which generates a quiz for the user to take and assess their knowledge in an easy and user-friendly manner.")
+    st.write("")
+    st.write("⚠️ Please expect 20 - 30 seconds for the quiz to be generated.")
     st.divider()
 
     # Initializing session states
